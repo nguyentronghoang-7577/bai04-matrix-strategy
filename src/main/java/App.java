eceec176ccec1414a28bbe1937bb118cdbea0f9f
nguyentronghoang-7577 <nguyentronghoang7577@gmail.com>
2026-04-28 10:59:22 +0700

    Fixed file separator to be distinct

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
+import java.io.File;
+
 public class App {
     public static String getFilePath(String folder, String fileName) {
-        return folder + "\\" + fileName;
+        return folder + File.separator + fileName;
     }
 }
